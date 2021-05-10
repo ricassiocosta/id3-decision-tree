571a39a95a08a9f6a7e9c5939b7c0afe569f81a3
ricassiocosta <ricassio_costa@outlook.com>
2021-05-10 15:49:00 -0300

    renaming some variables

--- src/utils/informationGain.ts
+++ src/utils/informationGain.ts
@@ -1,7 +1,7 @@
 // calculates the total information gain of a set based on a class set
 function getInformationGain(sampleSet: string[], classValuesSampleSet: string[]) {
   const totalEntropy = getTotalEntropy(classValuesSampleSet)
-  const obtainedEntropy = getGoalSampleSetEntropy(sampleSet, classValuesSampleSet)
+  const obtainedEntropy = getSampleSetEntropy(sampleSet, classValuesSampleSet)
 
   const informationGain = totalEntropy - obtainedEntropy
 
@@ -36,63 +36,63 @@ function getTotalEntropy(sampleSet: string[]) {
 }
 
 // calculates the obtained entropy of an sample set, based on a class set.
-function getGoalSampleSetEntropy(sampleSet: string[], classValuesSampleSet: string[]) {
-  const { classOccurrencesByGoalValue, goalOccurrences} = getSampleSetOccurrences(sampleSet, classValuesSampleSet)
-  const sampleSetGoalValueEntropy: any = {};
+function getSampleSetEntropy(sampleSet: string[], classValuesSampleSet: string[]) {
+  const { classValueOccurrencesBySampleSetValue, sampleSetValueOccurrences} = getOccurrences(sampleSet, classValuesSampleSet)
+  const sampleSetValueEntropy: any = {};
   const sampleSetValueProbability: any = {};
 
-  for (const goalValue in classOccurrencesByGoalValue) {
-    if (!(goalValue in sampleSetGoalValueEntropy)) {
-      sampleSetGoalValueEntropy[goalValue] = 0
+  for (const sampleSetValue in classValueOccurrencesBySampleSetValue) {
+    if (!(sampleSetValue in sampleSetValueEntropy)) {
+      sampleSetValueEntropy[sampleSetValue] = 0
     }
 
     let classValueTotalOccurrence = 0
-    let goalValueEntropy = 0
-    for (const classValue in classOccurrencesByGoalValue[goalValue]) {
-      const classValueOccurrence = classOccurrencesByGoalValue[goalValue][classValue]
+    let entropy = 0
+    for (const classValue in classValueOccurrencesBySampleSetValue[sampleSetValue]) {
+      const classValueOccurrence = classValueOccurrencesBySampleSetValue[sampleSetValue][classValue]
       classValueTotalOccurrence += classValueOccurrence
-      const probability = classValueOccurrence/goalOccurrences[goalValue]
-      goalValueEntropy += - (probability * Math.log2(probability))
+      const probability = classValueOccurrence/sampleSetValueOccurrences[sampleSetValue]
+      entropy += - (probability * Math.log2(probability))
     }
 
-    sampleSetValueProbability[goalValue] = classValueTotalOccurrence/sampleSet.length
-    sampleSetGoalValueEntropy[goalValue] = goalValueEntropy
+    sampleSetValueProbability[sampleSetValue] = classValueTotalOccurrence/sampleSet.length
+    sampleSetValueEntropy[sampleSetValue] = entropy
   }
 
   let obtainedEntropy = 0
-  for (const value in sampleSetGoalValueEntropy) {
-    obtainedEntropy +=  sampleSetValueProbability[value] * sampleSetGoalValueEntropy[value]
+  for (const value in sampleSetValueEntropy) {
+    obtainedEntropy +=  sampleSetValueProbability[value] * sampleSetValueEntropy[value]
   }
 
   return obtainedEntropy
 }
 
-// counts the number of occurrences of goal values and the occurrence of class values for each goal value 
-function getSampleSetOccurrences(sampleSet: string[], classValuesSampleSet: string[]) {
-  const classOccurrencesByGoalValue: any = {};
-  const goalOccurrences: any = {};
+// counts the number of occurrences each sample set value and the occurrence of class values for each sample set value 
+function getOccurrences(sampleSet: string[], classValuesSampleSet: string[]) {
+  const classValueOccurrencesBySampleSetValue: any = {};
+  const sampleSetValueOccurrences: any = {};
 
   sampleSet.forEach((value, index) => {
     const classValue = classValuesSampleSet[index]
 
-    if (!(value in classOccurrencesByGoalValue)) {
-      classOccurrencesByGoalValue[value] = {}
+    if (!(value in classValueOccurrencesBySampleSetValue)) {
+      classValueOccurrencesBySampleSetValue[value] = {}
     }
 
-    if (!(classValue in classOccurrencesByGoalValue[value])) {
-      classOccurrencesByGoalValue[value][classValue] = 0;
+    if (!(classValue in classValueOccurrencesBySampleSetValue[value])) {
+      classValueOccurrencesBySampleSetValue[value][classValue] = 0;
     }
 
-    classOccurrencesByGoalValue[value][classValue] += 1
+    classValueOccurrencesBySampleSetValue[value][classValue] += 1
 
-    if (!(value in goalOccurrences)) {
-      goalOccurrences[value] = 0;
+    if (!(value in sampleSetValueOccurrences)) {
+      sampleSetValueOccurrences[value] = 0;
     }
 
-    goalOccurrences[value] += 1
+    sampleSetValueOccurrences[value] += 1
   })
 
-  return { classOccurrencesByGoalValue, goalOccurrences}
+  return { classValueOccurrencesBySampleSetValue, sampleSetValueOccurrences}
 }
 
 export { getInformationGain }
